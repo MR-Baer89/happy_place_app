59d9d600f42a63a3ceba6508952ef4641674cab0
Blackbaer89 <d-mueller29@t-online.de>
2024-08-27 16:22:25 +0200

    Clubs

--- main.dart
+++ main.dart
@@ -1,27 +1,58 @@
 import 'dart:io';
 
-void Main() {
+List<String> clubs = [
+  "Cocktailba",
+  "Shishabar",
+  "Disco",
+  "Irishpub",
+  "Monkyclub"
+];
+
+void main() {
   // Registrieren/Account anlegen
   // Login
-  // Location Steckbrief
+  // Club Steckbrief
   print("Wilkommen bei HappyPlace");
   print(" MIt HappyPlace erleichterst du deine Freizeit Gestallung");
-  bool isProgramRunning = true;
+  bool isProgramRunning = false;
   while (isProgramRunning) {
-    print("Was möchtest du tun? Programm Exit, Login, Locationsteckbrief.");
+    print("Was möchtest du tun? Programm (E)xit, (L)ogin, (C)lubsteckbrief.");
     // Eigabe des Users
     String userChoiceInput = stdin.readLineSync()!;
 
     switch (userChoiceInput) {
-      case "exit" || "Exit":
-        isProgramRunning = false;
+      case "e" || "E":
+        isProgramRunning = true;
       // User soll sich mit Name und Passwort anmelden können.
-      case "login" || "Login":
+      case "l" || "L":
         stdout.write("Gib deinen Username ein:");
         stdin.readLineSync()!;
         stdout.write("Bitte Passwort eingeben");
         stdin.readLineSync();
+      // Auswahl des Clubs
+      case "c" || "C":
+        stdout.write("Willkommen zum Clubauswahlprogramm!");
+        stdout.write("Bitte wähle einen Club aus der folgenden Liste:");
     }
-    // Auswahl der Localität
+  }
+  for (int i = 0; i < clubs.length; i++) {
+    print('${i + 1}. ${clubs[i]}');
+  }
+
+  stdout.write('Gib die Nummer des gewünschten Clubs ein: ');
+  String? input = stdin.readLineSync();
+
+  if (input != null) {
+    int? choice = int.tryParse(input);
+
+    if (choice != null && choice > 0 && choice <= clubs.length) {
+      print('Du hast den ${clubs[choice - 1]} ausgewählt.');
+    } else {
+      print(
+          'Ungültige Auswahl. Bitte starte das Programm neu und wähle eine gültige Nummer.');
+    }
+  } else {
+    print(
+        'Ungültige Eingabe. Bitte starte das Programm neu und wähle eine gültige Nummer.');
   }
 }
